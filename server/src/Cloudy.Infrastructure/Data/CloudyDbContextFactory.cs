5374fbca13bece58123906ab1423a4cbcdae8ea1
Nathaniel Poppe <poppent@mail.uc.edu>
2025-10-07 16:43:23 -0400

    missed dbcontext factory somehow (#25)
    * cleanup, let appsettings be only populated by .env
    
    * better ef cmd usage with factory

--- server/src/Cloudy.Infrastructure/Data/CloudyDbContextFactory.cs
+++ server/src/Cloudy.Infrastructure/Data/CloudyDbContextFactory.cs
@@ -1,35 +1,21 @@
-using System.Reflection;
-using Cloudy.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
+namespace Cloudy.Infrastructure.Data;
 public class CloudyDbContextFactory : IDesignTimeDbContextFactory<CloudyDbContext>
 {
     public CloudyDbContext CreateDbContext(string[] args)
     {
-        // Find the path to the Cloudy.API project
-        var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
-        var projectRoot = Path.GetFullPath(Path.Combine(
-            exePath,
-            "..", "..", "..", "..",    // back out of bin/Debug/netX
-            "Cloudy.API"               // into the API project
-        ));
-
-        // Build configuration, but make both files optional
         var config = new ConfigurationBuilder()
-            .SetBasePath(projectRoot)
-            .AddJsonFile("appsettings.json") // could make optional: true
-            .AddJsonFile("appsettings.Development.json") // could make optional: true
             .AddEnvironmentVariables()
             .Build();
 
-        // Try JSON first, then environment 
-        var conn = config.GetConnectionString("DefaultConnection")
-                    ?? Environment.GetEnvironmentVariable("DefaultConnection")
-                    ?? throw new InvalidOperationException(
-                        "No connection string found. Place it in appsettings or set the DefaultConnection env var."
-                        );
+        var conn = config["DB_CONNSTRING"]
+                   ?? Environment.GetEnvironmentVariable("DB_CONNSTRING")
+                   ?? throw new InvalidOperationException(
+                       "No connection string found. Make sure the .env file was loaded correctly."
+                   );
 
         var builder = new DbContextOptionsBuilder<CloudyDbContext>();
         builder.UseNpgsql(conn);
